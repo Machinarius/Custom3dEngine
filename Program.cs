f6dac1779ac4812d73f0544abe19ba6d243a928a
German Valencia <machinarius92@gmail.com>
2023-05-02 17:43:32 -0500

    Figured out a janky way to make Renderdoc work on nvidia linux

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using Machinarius.Custom3dEngine.GLAbstractions;
+using Silk.NET.Maths;
 using Silk.NET.Windowing;
 
 namespace Machinarius.Custom3dEngine;
@@ -7,6 +8,11 @@ public class Program {
   public static void Main() {
     var options = WindowOptions.Default;
     options.Title = "My 3d engine";
+
+    if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("ENABLE_RENDERDOC_WORKAROUND"))) {
+      options.TransparentFramebuffer = true;
+      options.PreferredBitDepth = new Vector4D<int>(0);
+    }
 
     using var window = Window.Create(options);
     var renderer = new RenderConfigurator(window);
